fbaadda36e9f43fc26be252735fc1d5a4423de25
Анастасия <reverdini86@gmail.com>
2025-11-18 01:55:36 +0300

    Add files via upload

---
+++ for_rep333.py
@@ -0,0 +1,18 @@
+def is_password_good(password):
+    if len(password) < 8:
+        return False
+    a1 = False
+    a2 = False
+    a3 = False
+
+    for a in password:
+        if a.isupper():
+            a1 = True
+        elif a.islower():
+            a2 = True
+        elif a.isdigit():
+            a3 = True
+        return a1 and a2 and a3
+
+n = input()
+print(is_password_good(n))
